571f6c4f516406f0474bb361f0c14ab941e54031
dodeon101 <dodeon@101.inc>
2023-04-02 20:25:36 +0900

    init

---
+++ src/main/java/com/dopamine/slackapp/DopamineGirlsSlackAppApplication.java
@@ -0,0 +1,13 @@
+package com.dopamine.slackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineGirlsSlackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopamineGirlsSlackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dopamine/slackapp/DopamineGirlsSlackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopamine.slackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopamineGirlsSlackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
